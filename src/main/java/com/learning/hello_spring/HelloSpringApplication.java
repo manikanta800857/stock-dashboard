07b23d1b6a9132cedb72a1606da3d05b383edb2a
manikanta800857 <manikanta800857@gmail.com>
2025-08-26 16:50:01 +0530

    Update HelloSpringApplication.java

--- src/main/java/com/learning/hello_spring/HelloSpringApplication.java
+++ src/main/java/com/learning/hello_spring/HelloSpringApplication.java
@@ -3,11 +3,13 @@ package com.learning.hello_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// ADD THIS IMPORT:
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
+@EnableScheduling   // ADD THIS LINE TO ENABLE SCHEDULING
 public class HelloSpringApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
 }

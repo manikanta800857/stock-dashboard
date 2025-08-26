266376a1ac8122054729bc0f769024905c994035
manikanta800857 <manikanta800857@gmail.com>
2025-08-26 13:50:10 +0530

    Update StockController.java

--- src/main/java/com/learning/hello_spring/StockController.java
+++ src/main/java/com/learning/hello_spring/StockController.java
@@ -1,1 +1,26 @@
+package com.learning.hello_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/stocks")
+public class StockController {
+
+    private final IndianStockApiService indianStockApiService;
+
+    @Autowired
+    public StockController(IndianStockApiService indianStockApiService) {
+        this.indianStockApiService = indianStockApiService;
+    }
+
+    @GetMapping("/top")
+    public Object getTopStocks() {
+        return indianStockApiService.getTopPerformers();
+    }
+
+    @GetMapping("/losers")
+    public Object getTopLosers() {
+    return indianStockApiService.getTopPerformers(); // You'll need to implement this method
+}
+}

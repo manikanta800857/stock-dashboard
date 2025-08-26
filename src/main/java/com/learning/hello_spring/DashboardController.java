59215631a36f6dc82a57cbfae447a80d51e36f5a
manikanta800857 <manikanta800857@gmail.com>
2025-08-26 16:52:35 +0530

    Update DashboardController.java

--- src/main/java/com/learning/hello_spring/DashboardController.java
+++ src/main/java/com/learning/hello_spring/DashboardController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
-    
+
     @GetMapping("/")
     public String home() {
         return "dashboard";
     }
-    
+
     @GetMapping("/dashboard")
     public String dashboard() {
         return "dashboard";
